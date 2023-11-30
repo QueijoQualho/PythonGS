59b3da7bec8b49157a5714f6fe39b30af65f009b
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2023-11-30 15:49:24 -0300

    add more functions in menu

--- GS.py
+++ GS.py
@@ -1,38 +1,42 @@
-estabelecimentos_sp = [
+estabelecimentos_exemplo = [
     {
         "Nome": "Hospital A",
         "Endereço": "Rua 1, 123",
         "Número": "123",
         "Bairro": "Centro",
+        "Município": "São Paulo",
+        "Estado": "SP",
         "CEP": "12345-678",
         "Telefone": "(11) 1234-5678",
         "Email": "hospitala@example.com",
-        "Município": "São Paulo"
     },
     {
         "Nome": "Clínica B",
         "Endereço": "Av. 2, 456",
         "Número": "456",
         "Bairro": "Bela Vista",
+        "Município": "Campinas",
+        "Estado": "AM",
         "CEP": "98765-432",
         "Telefone": "(11) 9876-5432",
         "Email": "clinicab@example.com",
-        "Município": "Campinas"
-    },
+    }
 ]
-def validar_estado(estado):
-    estados_validos = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR",
-                       "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
 
-    return estado.upper() in estados_validos
+def validar_estado(estado):
+    return estado.upper() in ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR",
+                               "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
 
 def exibir_menu_principal():
-    print("\n=== Menu Health Spotter ===")
-    print("1. Buscar Serviços de Saúde Próximos")
-    print("2. Sair")
+    print("\n=== Menu Health Spotter === \n"
+          + "1. Buscar Serviços de Saúde Próximos \n"
+          + "2. Adicionar Estabelecimento \n" 
+          + "3. Remover Estabelecimento \n"
+          + "4. Sair")
+
 
 def buscar_estabelecimentos():
-    estado = input("Digite a sigla do estado (ex: SP): ")
+    estado = input("Digite a sigla do estado (ex: SP): ").upper()
 
     if not validar_estado(estado):
         print("Sigla de estado inválida.")
@@ -40,7 +44,7 @@ def buscar_estabelecimentos():
 
     municipio = input("Digite o nome do município: ")
 
-    resultados = [estabelecimento for estabelecimento in estabelecimentos_sp
+    resultados = [estabelecimento for estabelecimento in estabelecimentos_exemplo
                   if estabelecimento["Município"].lower() == municipio.lower()]
 
     if resultados:
@@ -52,18 +56,47 @@ def buscar_estabelecimentos():
     else:
         print(f"Nenhum estabelecimento encontrado em {municipio}, {estado}.")
 
+def adicionar_estabelecimento():
+    novo_estabelecimento = {}
+    for key in estabelecimentos_exemplo[0].keys():
+        if key != "Estado":
+            valor = input(f"Digite o(a) {key.lower()}: ")
+            novo_estabelecimento[key] = valor
+    novo_estabelecimento["Estado"] = input("Digite a sigla do estado: ")
+    estabelecimentos_exemplo.append(novo_estabelecimento)
+    print("Estabelecimento adicionado com sucesso!")
+    
+def remover_estabelecimento():
+    nome = str(input("Qual o nome do estabeleciemento: "))
+    
+    for i in estabelecimentos_exemplo:
+        if nome.lower() == i["Nome"].lower():
+            estabelecimentos_exemplo.remove(i)
+            print(f"Estabelecimento {nome} removido com sucesso.")
+            return
+    
+    print(f"Estabelecimento {nome} não encontrado.")
+
+
 def main():
     while True:
         exibir_menu_principal()
-        opcao = input("Digite o número da opção desejada: ")
-
-        if opcao == "1":
-            buscar_estabelecimentos()
-        elif opcao == "2":
-            print("Saindo do programa. Até logo!")
+        try:
+            opcao = input("Digite o número da opção desejada: ")
+            if opcao == "1":
+                buscar_estabelecimentos()
+            elif opcao == "2":
+                adicionar_estabelecimento()
+            elif opcao == "3":
+                remover_estabelecimento()
+            elif opcao == "4":
+                print("Saindo do programa. Até logo!")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+        except KeyboardInterrupt:
+            print("\nSaindo do programa. Até logo!")
             break
-        else:
-            print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     main()
